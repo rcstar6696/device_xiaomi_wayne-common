e66cb637446628ba5e1276aa76107a6051a91a31
Subhajeet Muhuri <kenny3fcb@gmail.com>
2020-02-07 16:35:34 +0530

    wayne-common: WAYNE_PATH -> LOCAL_PATH

--- wayne.mk
+++ wayne.mk
@@ -21,12 +21,9 @@
 # definition file).
 #
 
-
-WAYNE_PATH := device/xiaomi/wayne-common
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(WAYNE_PATH)/overlay
+    $(LOCAL_PATH)/overlay
 
 # Inherit from sdm660-common
 $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
@@ -36,10 +33,10 @@ $(call inherit-product, vendor/xiaomi/wayne-common/wayne-common-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(WAYNE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(WAYNE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(WAYNE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(WAYNE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
